49c5c09544f5aed177fee8db1037e0c2ffef51b2
Kaan Yilmaz <douscriptist@gmail.com>
2020-05-19 20:20:44 +0300

    User & Profile (Settings) Models

---
+++ server/src/models/Profile.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+	avatar: String,
+	isActive: { type: Boolean, default: true },
+	darkMode: { type: Boolean, default: false },
+	hideFavourites: { type: Boolean, default: false },
+	hideProfile: { type: Boolean, default: false },
+	movies: {
+		type: [mongoose.Schema.Types.ObjectId],
+		ref: 'movie',
+	},
+	series: {
+		type: [mongoose.Schema.Types.ObjectId],
+		ref: 'serie',
+	},
+	createdAt: { type: Date, default: Date.now },
+	updatedAt: { type: Date, default: Date.now },
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+});
+
+// Reverse populate with virtuals
+BootcampSchema.virtual('movies', {
+	ref: 'Movie',
+	localField: '_id',
+	foreignField: 'user.profile',
+	justOne: false,
+});
+
+// Reverse populate with virtuals
+BootcampSchema.virtual('series', {
+	ref: 'Serie',
+	localField: '_id',
+	foreignField: 'user.profile',
+	justOne: false,
+});
+
+module.exports = UserSetting = mongoose.model('Profile', ProfileSchema);
