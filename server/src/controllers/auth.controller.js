a5faf6b49f2b87eb6c84edfdb63bc39966b50e4d
Kaan Yilmaz <douscriptist@gmail.com>
2020-06-07 01:47:22 +0300

    user routes & controller refactor ~~

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -2,85 +2,109 @@ const CustomError = require('../utils/CustomError');
 const asyncHandler = require('../middlewares/asyncHandler');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { check, validationResult } = require('express-validator');
-
+//LATER: REMOVE EXPRESS VALIDATOR
 const User = require('../models/User');
 
-// @route   GET douvies/auth
-// @desc    Test Route
-// @access  Public
-exports.getLoggedInUser = asyncHandler(async (req, res) => {
-	try {
-		const user = await User.findById(req.user.id).select('-password');
-		res.json(user);
-	} catch (err) {
-		console.error(err.message);
-		res.status(500).send('Server Error');
-	}
+// @desc      Register user -> Get Token
+// @route     POST /api/v1/auth/register
+// @access    Public
+exports.register = asyncHandler(async (req, res, next) => {
+	const { name, username, email, password } = req.body;
+
+	// Create User
+	const user = await User.create({
+		name,
+		username,
+		email,
+		password,
+	});
+
+	tokenResponse(user, 200, res);
 });
 
-// @route   POST douvies/auth
-// @desc    Authenticate User & Get Token
-// @access  Public
-exports.login = asyncHandler(async (req, res) => {
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		return res.status(400).json({ errors: errors.array() });
-	}
-	const { input, password } = req.body;
-
-	try {
-		// See if user exists?
-		let user = await User.findOne({ email: input });
-		let userName = await User.findOne({ username: input });
-		if (!user) {
-			if (!userName) {
-				// Security ????
-				return res
-					.status(404)
-					.json({ errors: [{ success: false, msg: 'User not found!' }] });
-			} else {
-				user = userName;
-			}
-		}
-
-		// Match email and password
-		const isMatched = await bcrypt.compare(password, user.password);
-		if (!isMatched) {
-			// Security ????
-			return res
-				.status(404)
-				.json({ errors: [{ success: false, msg: 'Password is wrong!' }] });
-		}
-
-		// req.user.id or req.id
-		// const payload = {
-		// 	user: {
-		// 		id: user.id,
-		// 	},
-		// };
-		const payload = { id: user.id };
-
-		jwt.sign(
-			payload,
-			process.env.JWT_SECRET,
-			// LATER:
-			{ expiresIn: 360000 },
-			(err, token) => {
-				if (err) throw err;
-				res.json({ success: true, token: token });
-			}
+// @desc      Login user -> Get Token
+// @route     POST /api/v1/auth/login
+// @access    Public
+exports.login = asyncHandler(async (req, res, next) => {
+	const { username, email, password } = req.body;
+	// Validate email & password
+	if ((!email && !username) || !password) {
+		return next(
+			new CustomError('Username or Email & password is required.', 400)
 		);
+	}
+
+	// Check if user exists
+	// with email or username
+	let user;
+	if (!username) {
+		user = await User.findOne({ email }).select('+password');
+	} else {
+		user = await User.findOne({ username }).select('+password');
+	}
+
+	if (!user) {
+		return next(new CustomError('User not found.', 401));
+	}
+
+	// Check password is matching
+	const isMatched = await user.isMatchedPassword(password);
+	if (!isMatched) {
+		return next(new CustomError('Password is wrong!', 401));
+	}
+
+	tokenResponse(user, 200, res);
+});
+
+// @desc      Logout user & Clear cookie
+// @route     GET /api/v1/auth/logout
+// @access    Private
+exports.logout = asyncHandler(async (req, res, next) => {
+	res.cookie('token', 'none', {
+		expires: new Date(Date.now() + 10 * 1000),
+		httpOnly: true,
+	});
+
+	res.status(200).json({
+		success: true,
+		data: {},
+	});
+});
+
+// @desc      Get current logged in user
+// @route     GET /api/v1/auth/me
+// @access    Private
+exports.getMe = asyncHandler(async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id);
+		res.status(200).json({
+			success: true,
+			data: user,
+		});
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send('Server Error');
 	}
 });
 
-exports.check = [
-	check('input', 'Username or email is required').exists(),
-	check('input', 'Username has to be minimum 6 characters').isLength({
-		min: 6,
-	}),
-	check('password', 'Password is required!').exists(),
-];
+// Get Token from model, create cookie and send response
+const tokenResponse = (user, statusCode, res) => {
+	// Create Token // Comes from User model as methods not statics
+	const token = user.getSignedJWTToken();
+
+	const options = {
+		expires: new Date(
+			Date.now() + process.env.JWT_COOKIE_EXPIRE_TIME * 24 * 60 * 60 * 1000
+		),
+		httpOnly: true,
+	};
+
+	if (process.env.NODE_ENV === 'production') {
+		options.secure = true;
+	}
+
+	res.status(statusCode).cookie('token', token, options).json({
+		success: true,
+		token,
+	});
+};
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const {
-	check,
-	getLoggedInUser,
+	register,
 	login,
+	getMe,
+	logout,
 } = require('../controllers/auth.controller');
 
 const { protect, authorize } = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.get('/', protect, getLoggedInUser);
-router.post('/', check, login);
+// router.get('/', protect, getLoggedInUser);
+router.get('/me', protect, getMe);
+router.get('/logout', protect, logout);
+
+router.post('/login', login);
+router.post('/register', register);
 
 module.exports = router;
