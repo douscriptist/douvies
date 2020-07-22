191030d02671faff1dc29e36d9fce5069eb3f1cf
Kaan Yilmaz <douscriptist@gmail.com>
2020-07-22 23:11:18 +0300

    into the auth state ~~

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import theme from './theme';
 import alert from './alert';
+import auth from './auth';
 
 export default combineReducers({
 	theme,
 	alert,
+	auth,
 });
